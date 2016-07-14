0a22cf481f433093acd3f28fb8e9690d2056b610
James Reichard <jpqreichard@gmail.com>
2016-07-14 16:13:54 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function logNumber(){
+  var number = 5
+  console.log( number );
+}
+
+function logNumberWithParam(number){
+  console.log( number );
+}
+
+logNumber();
+
+logNumberWithParam(6);
+
+console.log(number);
